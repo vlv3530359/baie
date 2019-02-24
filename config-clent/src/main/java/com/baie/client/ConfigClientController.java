de04721d760ccc300f98a5d3b53cfba43b71c0e7
vlv <3530359@qq.com>
2019-02-24 22:56:44 +0800

    master

--- config-clent/src/main/java/com/baie/client/ConfigClientController.java
+++ config-clent/src/main/java/com/baie/client/ConfigClientController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/client")
 public class ConfigClientController {
-    @Value("${spring.kafka.consumer.group-id}")
+    @Value("${test.mo}")
     private String testValue;
 
     @GetMapping("test")
